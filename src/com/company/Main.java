a33da7205b59a255e57ad06dcd9317d1c99414b5
VERONICA-RESTREPO <veronica.restrepo@globant.com>
2021-06-03 20:33:28 -0500

    rama1: modificación de .gitignore y creación proyecto

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Este es un ejemplo para el módulo de git");
+        System.out.println("Cada que se realice un cambio estos mensajes se mostrarán");
+    }
+}
