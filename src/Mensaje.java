b876ff64deb5a2dbb9438ea7ad27b1c3e23f998b
VERONICA-RESTREPO <veronica.restrepo@globant.com>
2021-06-03 22:45:35 -0500

    creacion Mensaje.java

---
+++ src/Mensaje.java
@@ -0,0 +1,8 @@
+public class Mensaje {
+
+    public static void main(String[] args) {
+
+        System.out.println("Este es un ejemplo para el módulo de git");
+        System.out.println("Cada que se realice un cambio estos mensajes se mostrarán");
+    }
+}
